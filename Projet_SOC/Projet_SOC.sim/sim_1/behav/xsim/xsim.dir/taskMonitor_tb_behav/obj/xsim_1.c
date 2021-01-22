0ae10129762cac85e93d5267ab48b37c95bc5297
William Psc <pensec.william@gmail.com>
2021-01-22 22:31:09 +0100

    1.2
    Rendu du projet !

--- Projet_SOC/Projet_SOC.sim/sim_1/behav/xsim/xsim.dir/taskMonitor_tb_behav/obj/xsim_1.c
+++ Projet_SOC/Projet_SOC.sim/sim_1/behav/xsim/xsim.dir/taskMonitor_tb_behav/obj/xsim_1.c
@@ -62,8 +62,8 @@ const int NumRelocateId= 12;
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/taskMonitor_tb_behav/xsim.reloc",  (void **)funcTab, 12);
-	iki_vhdl_file_variable_register(dp + 6592);
-	iki_vhdl_file_variable_register(dp + 6648);
+	iki_vhdl_file_variable_register(dp + 6624);
+	iki_vhdl_file_variable_register(dp + 6680);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
